aa98d5dda43b8f5f2e3311e4d5b1cd8992d18180
MaoLongLong <382084620@qq.com>
2021-08-25 19:49:06 +0800

    add HOST config

--- handler/handlers.go
+++ handler/handlers.go
@@ -2,11 +2,21 @@ package handler
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/maolonglong/tinyurl/shortener"
 	"github.com/maolonglong/tinyurl/storage"
 )
+
+var host string
+
+func init() {
+	host = os.Getenv("HOST")
+	if host == "" {
+		host = "http://localhost:9808/"
+	}
+}
 
 type UrlCreationRequest struct {
 	LongUrl string `json:"long_url" binding:"required"`
@@ -24,7 +34,6 @@ func CreateShortUrl(c *gin.Context) {
 	shortUrl := shortener.GenerateShortLink(creationRequest.LongUrl)
 	storage.SaveUrlMapping(shortUrl, creationRequest.LongUrl)
 
-	host := "http://localhost:9808/"
 	c.JSON(http.StatusOK, gin.H{
 		"message":   "short url created successfully",
 		"short_url": host + shortUrl,
@@ -34,5 +43,11 @@ func CreateShortUrl(c *gin.Context) {
 func HandleShortUrlRedirect(c *gin.Context) {
 	shortUrl := c.Param("shortUrl")
 	initialUrl := storage.RetrieveInitialUrl(shortUrl)
+	if initialUrl == "" {
+		c.JSON(http.StatusNotFound, gin.H{
+			"message": "page not found",
+		})
+		return
+	}
 	c.Redirect(http.StatusFound, initialUrl)
 }
